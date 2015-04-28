fadea1b642f6575182c9a5dd59a65759db8e818e
xianmau <xianmaulin@gmail.com>
2015-04-28 16:30:22 +0800

    add resume

---
+++ xianmau.me/Areas/Resume/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace xianmau.me.Areas.Resume.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Resume/Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ xianmau.me/Areas/Resume/ResumeAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace xianmau.me.Areas.Resume
+{
+    public class ResumeAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Resume";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Resume_default",
+                "Resume/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional },
+                new string[] { "xianmau.me.Areas.Resume.Controllers" }
+            );
+        }
+    }
+}
