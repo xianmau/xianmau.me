9e0e2778618bc2f65b56910c486d543f1a4b2694
xianmau <xianmaulin@gmail.com>
2015-09-07 14:54:05 +0800

    add log

--- xianmau.me/Areas/Resume/Controllers/HomeController.cs
+++ xianmau.me/Areas/Resume/Controllers/HomeController.cs
@@ -3,18 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using xianmau.me.Models;
 
 namespace xianmau.me.Areas.Resume.Controllers
 {
     public class HomeController : Controller
     {
+        private XMDB db = new XMDB();
+
         //
         // GET: /Resume/Home/
 
         public ActionResult Index()
         {
+            Log log = new Log();
+
+            string userIp = System.Web.HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+            if (string.IsNullOrEmpty(userIp))
+            {
+                userIp = System.Web.HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
+
+            }
+
+            log.Ip = userIp;
+            log.Action = string.Format("visit: {0}", System.Web.HttpContext.Current.Request.RawUrl);
+            log.Time = DateTime.Now;
+
+            if (ModelState.IsValid)
+            {
+                db.logs.Add(log);
+                db.SaveChanges();
+            }
+
             return View();
         }
 
     }
+
 }
