0530d618f49943a3591a98f02ee0753e72421d30
xianmau <xianmaulin@gmail.com>
2015-09-07 16:00:04 +0800

    add log

---
+++ xianmau.me/Areas/Admin/Controllers/LogController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using xianmau.me.Models;
+
+namespace xianmau.me.Areas.Admin.Controllers
+{
+    public class LogController : Controller
+    {
+        private XMDB db = new XMDB();
+
+        //
+        // GET: /Admin/Log/
+
+        public ActionResult Index()
+        {
+            return View(db.logs.OrderByDescending(i => i.Time));
+        }
+
+    }
+}
